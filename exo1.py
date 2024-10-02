2243306a4f033a189e75d154038589ad09708ccc
Enzo Mauclair <enzo.mauclair@gmail.com>
2024-10-02 16:13:33 +0200

    exo3

--- exo1.py
+++ exo1.py
@@ -1,5 +1,12 @@
 #age = 24
 #print(age)
+#nom = "mauclair"
+#print(nom)
 
-nom = "mauclair"
-print(nom)
+num1 = 1
+num2 = 2
+
+num1, num2 = num2, num1
+
+print("Voici les valeurs, num1 =", num1)
+print("Voici les valeurs, num2 =", num2)
