3065ebcf47cb6866f7497bc916128af5602b06f7
Enzo Mauclair <enzo.mauclair@gmail.com>
2024-10-02 16:49:16 +0200

    partie2 exo2

--- exo2.py
+++ exo2.py
@@ -1,8 +1,11 @@
-nombre = int(input("Votre nombre : "))
+#nombre = int(input("Votre nombre : "))
 
-if nombre > 0:
-    print("Le nombre est +")
-elif nombre < 0:
-    print("Le nombre est -")
-else:
-    print("Le nombre est égale à 0")
+#if nombre > 0:
+#    print("Le nombre est +")
+#elif nombre < 0:
+#    print("Le nombre est -")
+#else:
+#    print("Le nombre est égale à 0")
+
+for num in range(1,6) :
+   print(num)
